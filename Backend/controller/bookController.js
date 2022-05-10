43b72909acc8aa8588c56f1cb2cc71826a857ca5
Paul Hargens <hargens@hm.edu>
2022-05-10 19:12:46 +0200

    Added Book Router/ Controller

---
+++ Backend/controller/bookController.js
@@ -0,0 +1,136 @@
+const log = require('../helpers/logger')
+const apiResponse = require('../helpers/apiResponse')
+const bookModel = require('../models/DatabaseModel')
+
+exports.addBook = [
+  async (req, res) => {
+    log('Controller.bookController.addBook - Start', 'debug')
+    const { name, author, releaseDate, comment, genreID, studentID } = req.body
+    let book = new bookModel({
+      name,
+      author,
+      releaseDate,
+      comment,
+      genreID,
+      studentID,
+    })
+    let newBook = await book.save().catch((err) => {
+      log(
+        'Controller.bookController.addBook - Failed to add book: ' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.bookController.addBook - End', 'debug')
+    return apiResponse.successResponseWithData(res, 'Book created', newBook)
+  },
+]
+
+exports.editBook = [
+  async (req, res) => {
+    log('Controller.bookController.editBook - Start', 'debug')
+    let book = await bookModel
+      .findByIdAndUpdate(req.body._id, req.body)
+      .catch((err) => {
+        log(
+          'Controller.bookController.editBook - Failed to edit Book ' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+
+    log('Controller.bookController.editBook - End', 'debug')
+    return apiResponse.successResponseWithData(res, 'Book edited', book)
+  },
+]
+
+exports.deleteBook = [
+  async (req, res) => {
+    log('Controller.bookController.deleteBook - Start ', 'debug')
+    await bookModel.findByIdAndDelete(req.body.id).catch((err) => {
+      log(
+        'Controller.bookController.deleteBook - Failed to delete book: ' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+
+    log('Controller.bookController.editBook - End', 'debug')
+    return apiResponse.successResponse(res, 'Book edited')
+  },
+]
+
+exports.getAllBooks = [
+  async (req, res) => {
+    log('Controller.bookController.getAllBooks - Start', 'debug')
+    let allBooks = await bookModel.find().catch((err) => {
+      log(
+        'Controller.bookController.getAllBooks - Failed while searching for books' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.bookController.getAllBooks - End', 'debug')
+    return apiResponse.successResponseWithData(res, 'Books found', allBooks)
+  },
+]
+
+exports.getBookByID = [
+  async (req, res) => {
+    log('Controller.bookController.getBookByID - Start', 'debug')
+    let book = await bookModel.findById(req.body._id).catch((err) => {
+      log(
+        'Controller.bookController.getBookByID - Failed while searching for the book with the id: ' +
+          req.body._id +
+          '. Error Message is' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.bookController.getBookByID - END ', 'debug')
+    return apiResponse.successResponseWithData(res, 'Book found', book)
+  },
+]
+
+exports.getBookByName = [
+  async (req, res) => {
+    log('Controller.bookController.getBookByName - Start', 'debug')
+    let book = await bookModel.find({ name: req.body.name }).catch((err) => {
+      log(
+        'Controller.bookController.getBookByName - Failed while searching for the book with the name: ' +
+          req.body.name +
+          '. Error Message is' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.bookController.getBookByName - END ', 'debug')
+    return apiResponse.successResponseWithData(res, 'Book found', book)
+  },
+]
+
+exports.getAllBooksOfOneGenre = [
+  async (req, res) => {
+    log('Controller.bookController.getAllBooksOfOneGenre - Start', 'debug')
+    let books = await bookModel
+      .find({ genreID: req.body.genreID })
+      .catch((err) => {
+        log(
+          'Controller.bookController.getAllBooksOfOneGenre - Failed while searching for books with the Genre id: ' +
+            req.body.genreID +
+            '. Error Message is' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+    log('Controller.bookController.getAllBooksOfOneGenre - END ', 'debug')
+    return apiResponse.successResponseWithData(res, 'Books found', books)
+  },
+]
--- Backend/routes/bookRouter.js
+++ Backend/routes/bookRouter.js
@@ -1,8 +1,40 @@
 const express = require('express')
-const {checkStudent, checkTeacher} = require('../middleware/index')
+const middleware = require('../middleware/index')
+const bookController = require('../controller/bookController')
 
 const bookRouter = express.Router({
-    mergeParams: true
+  mergeParams: true,
 })
+
+bookRouter.post('/addBook', middleware.checkTeacher, bookController.addBook)
+
+bookRouter.post('/editBook', middleware.checkTeacher, bookController.editBook)
+
+bookRouter.delete(
+  '/deleteBook',
+  middleware.checkTeacher,
+  bookController.deleteBook
+)
+
+bookRouter.get(
+  '/getAllBooks',
+  middleware.checkStudent,
+  bookController.getAllBooks
+)
+bookRouter.get(
+  '/getBookByID',
+  middleware.checkStudent,
+  bookController.getBookByID
+)
+bookRouter.get(
+  '/getBookByName',
+  middleware.checkStudent,
+  bookController.getBookByName
+)
+bookRouter.get(
+  '/getBoogetAllBooksOfOneGenreByID',
+  middleware.checkStudent,
+  bookController.getAllBooksOfOneGenre
+)
 
 module.exports.default = bookRouter
