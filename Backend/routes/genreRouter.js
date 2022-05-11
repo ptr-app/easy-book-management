ffdc090eabf4c9d9d4b328465a108461a1b544c1
Paul Hargens <hargens@hm.edu>
2022-05-11 14:48:30 +0200

    Added Genre Controller and Router

--- Backend/routes/genreRouter.js
+++ Backend/routes/genreRouter.js
@@ -1,8 +1,35 @@
 const express = require('express')
-const {checkStudent, checkTeacher} = require('../middleware/index')
+const middleware = require('../middleware/index')
+const genreController = require('../controller/genreController')
 
 const genreRouter = express.Router({
-    mergeParams: true
+  mergeParams: true,
 })
+
+classRouter.post('/addGenre', middleware.checkTeacher, genreController.addGenre)
+
+classRouter.post(
+  '/editGenre',
+  middleware.checkTeacher,
+  genreController.editGenre
+)
+
+classRouter.delete(
+  '/deleteGenre',
+  middleware.checkTeacher,
+  genreController.deleteGenre
+)
+
+classRouter.get(
+  '/getAllGenres',
+  middleware.checkTeacher,
+  genreController.getAllGenres
+)
+
+classRouter.get(
+  '/getGenreByID',
+  middleware.checkTeacher,
+  genreController.getGenreByID
+)
 
 module.exports.default = genreRouter
