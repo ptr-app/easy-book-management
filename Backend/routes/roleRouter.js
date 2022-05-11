f95f48cd159acc3f59f040a7488863085535aff0
Paul Hargens <hargens@hm.edu>
2022-05-11 15:03:24 +0200

    Added Roule Router and Controller

--- Backend/routes/roleRouter.js
+++ Backend/routes/roleRouter.js
@@ -1,8 +1,31 @@
 const express = require('express')
-const {checkStudent, checkTeacher} = require('../middleware/index')
+const middleware = require('../middleware/index')
+const roleController = require('../controller/roleController')
 
 const roleRouter = express.Router({
-    mergeParams: true
+  mergeParams: true,
 })
+
+classRouter.post('/addRole', middleware.checkDean, roleController.addRole)
+
+classRouter.post('/editRole', middleware.checkDean, roleController.editRole)
+
+classRouter.delete(
+  '/deleteRole',
+  middleware.checkDean,
+  roleController.deleteRole
+)
+
+classRouter.get(
+  '/getAllRoles',
+  middleware.checkTeacher,
+  roleController.getAllRoles
+)
+
+classRouter.get(
+  '/getRoleByID',
+  middleware.checkTeacher,
+  roleController.getRoleByID
+)
 
 module.exports.default = roleRouter
