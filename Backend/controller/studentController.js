4f1ca24ca7b7347ce8ae6dc08394f125d9583e2e
Paul Hargens <hargens@hm.edu>
2022-05-11 16:27:32 +0200

    Added Student Router / Controller

---
+++ Backend/controller/studentController.js
@@ -0,0 +1,208 @@
+const bcrypt = require('bcrypt')
+const log = require('../helpers/logger')
+const apiResponse = require('../helpers/apiResponse')
+const studentModel = require('../models/DatabaseModel')
+
+exports.addStudent = [
+  async (req, res) => {
+    log('Controller.studentController.addStudent - Start', 'debug')
+    const { name, birthdate, classID, bookID } = req.body
+    let passwordHash = await bcrypt.hash(req.body.password, 10)
+    delete req.body.password
+    let student = new studentModel({
+      name,
+      passwordHash,
+      classID,
+      bookID,
+    })
+    let newStudent = await student.save().catch((err) => {
+      log(
+        'Controller.studentController.addStudent - Failed to add student: ' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.studentController.addStudent - End', 'debug')
+    return apiResponse.successResponseWithData(
+      res,
+      'STUDENT_CREATED',
+      newStudent
+    )
+  },
+]
+
+exports.editStudent = [
+  async (req, res) => {
+    log('Controller.studentController.editStudent - Start', 'debug')
+    let passwordHash = await bcrypt.hash(req.body.password, 10)
+    delete req.body.password
+    delete req.body.classID
+    delete req.body.bookID
+    req.body.passwordHash = passwordHash
+    let student = await studentModel
+      .findByIdAndUpdate(req.body._id, req.body)
+      .catch((err) => {
+        log(
+          'Controller.studentController.editStudent - Failed to edit Student ' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+
+    log('Controller.studentController.editStudent - End', 'debug')
+    return apiResponse.successResponseWithData(res, 'STUDENT_EDITED', student)
+  },
+]
+
+exports.editStudentClassID = [
+  async (req, res) => {
+    log('Controller.studentController.editStudentClassID - Start', 'debug')
+    let classID
+    if (req.body.classID !== undefined) {
+      classModel.findById(req.body.classID).catch((err) => {
+        log(
+          'Controller.studentController.editStudentClassID - Failed to find Class ' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+
+      classID = req.body.classID
+    } else {
+      log(
+        'Controller.studentController.editStudentClassID - A Student needs an attached Class ',
+        'error'
+      )
+      return apiResponse.errorResponse(res, 'STUDENT_NEED_CLASS')
+    }
+
+    let student = await studentModel
+      .findByIdAndUpdate(req.body._id, { classID: req.body.classID })
+      .catch((err) => {
+        log(
+          'Controller.studentController.editStudentClassID - Failed to edit Student ' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+
+    log('Controller.studentController.editStudentClassID - End', 'debug')
+    return apiResponse.successResponseWithData(
+      res,
+      'STUDENT_ASSIGNED_CLASS',
+      student
+    )
+  },
+]
+
+exports.editStudentBookID = [
+  async (req, res) => {
+    log('Controller.studentController.editStudentBookID - Start', 'debug')
+    let bookID
+    if (req.body.bookID !== undefined) {
+      classModel.findById(req.body.bookID).catch((err) => {
+        log(
+          'Controller.studentController.editStudentBookID - Failed to find Book ' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+      bookID = req.body.bookID
+    } else {
+      bookID = null
+    }
+
+    let student = await studentModel
+      .findByIdAndUpdate(req.body._id, { bookID: req.body.bookID })
+      .catch((err) => {
+        log(
+          'Controller.studentController.editStudentBookID - Failed to edit Student ' +
+            err.message,
+          'error'
+        )
+        return apiResponse.errorResponse(res, err.message)
+      })
+
+    log('Controller.studentController.editStudentBookID - End', 'debug')
+    return apiResponse.successResponseWithData(
+      res,
+      'STUDENT_ASSIGNED_BOOK',
+      student
+    )
+  },
+]
+
+exports.deleteStudent = [
+  async (req, res) => {
+    log('Controller.studentController.deleteStudent - Start ', 'debug')
+    let book = bookModel.findById({ studentID: req.body._id }).catch((err) => {
+      log(
+        'Controller.studentController.deleteStudent - Failed to find class: ' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    if (book.length > 0) {
+      log(
+        'Controller.studentController.deleteStudent - Failed to delete student with Book attached to it: ',
+        'error'
+      )
+      return apiResponse.errorResponse(res, 'STUDENT_HAS_BOOK')
+    }
+    await studentModel.findByIdAndDelete(req.body._id).catch((err) => {
+      log(
+        'Controller.studentController.deleteStudent - Failed to delete student: ' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+
+    log('Controller.studentController.deleteStudent - End', 'debug')
+    return apiResponse.successResponse(res, 'STUDENT_DELETED')
+  },
+]
+
+exports.getAllStudents = [
+  async (req, res) => {
+    log('Controller.studentController.getAllStudents - Start', 'debug')
+    let allStudents = await studentModel.find().catch((err) => {
+      log(
+        'Controller.studentController.getAllStudents - Failed while searching for Students' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.studentController.getAllStudents - End', 'debug')
+    return apiResponse.successResponseWithData(
+      res,
+      'SCHOOLES_FOUND',
+      allStudents
+    )
+  },
+]
+
+exports.getStudentByID = [
+  async (req, res) => {
+    log('Controller.studentController.getStudentByID - Start', 'debug')
+    let student = await studentModel.findById(req.body._id).catch((err) => {
+      log(
+        'Controller.studentController.getStudentByID - Failed while searching for the student with the id: ' +
+          req.body._id +
+          '. Error Message is' +
+          err.message,
+        'error'
+      )
+      return apiResponse.errorResponse(res, err.message)
+    })
+    log('Controller.studentController.getStudentByID - END ', 'debug')
+    return apiResponse.successResponseWithData(res, 'SCHOOL_FOUND', student)
+  },
+]
--- Backend/routes/studentRouter.js
+++ Backend/routes/studentRouter.js
@@ -1,8 +1,51 @@
 const express = require('express')
-const {checkStudent, checkTeacher} = require('../middleware/index')
+const middleware = require('../middleware/index')
+const studentController = require('../controller/studentController')
 
 const studentRouter = express.Router({
-    mergeParams: true
+  mergeParams: true,
 })
+
+studentRouter.post(
+  '/addStudent',
+  middleware.checkTeacher,
+  studentController.addStudent
+)
+
+studentRouter.post(
+  '/editStudent',
+  middleware.checkTeacher,
+  studentController.editStudent
+)
+
+studentRouter.post(
+  '/editStudentClassID',
+  middleware.checkTeacher,
+  studentController.editStudentClassID
+)
+
+studentRouter.post(
+  '/editStudentBookID',
+  middleware.checkStudent,
+  studentController.editStudentBookID
+)
+
+studentRouter.delete(
+  '/deleteStudent',
+  middleware.checkTeacher,
+  studentController.deleteStudent
+)
+
+studentRouter.get(
+  '/getAllStudents',
+  middleware.checkTeacher,
+  studentController.getAllStudents
+)
+
+studentRouter.get(
+  '/getStudentByID',
+  middleware.checkTeacher,
+  studentController.getStudentByID
+)
 
 module.exports.default = studentRouter
