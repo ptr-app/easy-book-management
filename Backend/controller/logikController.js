ffa051578c0673352d8fe526301d05cb139a6398
Paul Hargens <hargens@hm.edu>
2022-05-16 19:17:16 +0200

    Created Logik Router/ Controller, inactive atm

---
+++ Backend/controller/logikController.js
@@ -0,0 +1,13 @@
+const log = require('../helpers/logger')
+const apiResponse = require('../helpers/apiResponse')
+const classModel = require('../models/DatabaseModel')
+const studentModel = require('../models/DatabaseModel')
+
+exports.advanceAllStudentsFromClass = [
+  async (req, res) => {
+    log(
+      'Controller.logikController.advanceAllStudentsFromClass - Start',
+      'debug'
+    )
+  },
+]
